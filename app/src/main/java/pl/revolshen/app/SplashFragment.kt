2dfe818e632ce42e43098b45787a955a69692b07
Czr4Ny213 <mikolaj.kolek2@gmail.com>
2023-05-30 22:24:02 +0200

    finished project

---
+++ app/src/main/java/pl/revolshen/app/SplashFragment.kt
@@ -0,0 +1,58 @@
+package pl.revolshen.app
+
+import android.content.Context
+import android.media.Image
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+
+class SplashFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            if(onBoardingIsFinished()){
+                findNavController().navigate(R.id.navigate_splashFragment_to_homeFragment)
+            }else{
+                findNavController().navigate(R.id.action_splashFragment_to_onBoardingFragment)
+            }
+
+
+        }, 3000)
+
+        val view = inflater.inflate(R.layout.fragment_splash, container, false)
+
+        val animLogo = AnimationUtils.loadAnimation(view.context, R.anim.splash_logo)
+        val animText = AnimationUtils.loadAnimation(view.context, R.anim.splash_text)
+        val signText = AnimationUtils.loadAnimation(view.context, R.anim.splas_sign)
+
+        val tvSplash = view.findViewById<TextView>(R.id.tv_splash)
+        val imgSplash = view.findViewById<ImageView>(R.id.imageView)
+        val signSplash = view.findViewById<TextView>(R.id.sign_splash)
+
+        tvSplash.animation = animText
+        imgSplash.animation = animLogo
+        signSplash.animation = signText
+
+        return view
+    }
+
+    private fun onBoardingIsFinished(): Boolean{
+
+        val sharedPreferences = requireActivity().getSharedPreferences("onBoarding", Context.MODE_PRIVATE)
+        return sharedPreferences.getBoolean("finished", false)
+    }
+}
